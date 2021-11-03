855fbf5c307e68a7c981e4be690b6add0b211619
dojin8381@gmail.com <dojin8381@gmail.com>
2021-11-03 10:31:24 +0900

    TEST용
    TEST 용 코드 수정

--- Goldenplanet_renewal/SceneDelegate.swift
+++ Goldenplanet_renewal/SceneDelegate.swift
@@ -13,13 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-//        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.makeKeyAndVisible()
 
         //전자상거래 관련 코드
         let product1 = NSMutableDictionary.init()
